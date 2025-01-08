26ffd4286ceda5a77d0bb3768225de540e1144d8
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2025-01-08 19:18:36 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+#include <vector>
+int main() 
+{
+    vector<string>v(5);
+    vector<string>w(5);
+    int i, a, contap, g;
+    string cantante;
+    vector<int>p(10);
+    i=0;
+    a=0;
+    g=0;
+    contap=0;
+    v {"caparezza", "gemitaiz", "thasup", "ghali", "drake"} ;
+    while (i<5)
+    {
+        cout << "come si chiama il cantante" << endl;
+        cin >> cantante;
+        w[i]= cantante;
+        i=i+1;
+    }
+    while(a<9)
+    {
+        n[a] = a+1;
+        a=a+1;
+    }
+    while (g<9)
+    {
+        p[contap] = contap+1;
+        if (contap%2=0)
+        {
+            g =g+1;
+            contap = contap +1;
+        }
+        else
+        {
+            contap =contap+1;
+        }
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
